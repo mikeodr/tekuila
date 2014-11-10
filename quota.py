4a3df0f3244de817a5e1c047f81e716bca27d7f2
modriscoll <modriscoll@blackberry.com>
2014-11-09 20:02:02 -0500

    Pulling and print stats with -v flag.

--- quota.py
+++ quota.py
@@ -1,42 +1,64 @@
 #!/usr/bin/env python
 
-import urllib2
 import json
 import ConfigParser
 import argparse
+import httplib
 
+API_URL = '/web/Usage/UsageSummaryRecords?$filter=IsCurrent%20eq%20true'
 
-def checkCap():
+def checkCap(args):
+    config = ConfigParser.ConfigParser()
+    APIKEY = None
+    # cap = None
+    # warn = None
+    if args.config is None:
+        pass
+
+    if args.API is None:
+        APIKEY = args.API
+    if args.cap is None:
+        pass
+    if args.warn is None:
+        pass
+
     APIKEY = "75C4166BF10C74715DB7E2B3BE365325"
-    proto = 'https://'
-    host = 'api.teksavvy.com'
-    path = "/web/Usage/UsageSummaryRecords?$filter=IsCurrent%20eq%20true"
-    url = proto + host + path
 
-    request = urllib2.Request(url)
-    request.add_header('TekSavvy-APIKey', APIKEY)
+    headers = {"TekSavvy-APIKey": APIKEY}
+    conn = httplib.HTTPSConnection("api.teksavvy.com")
+    conn.request('GET',
+                 API_URL,
+                 '', headers)
+    response = conn.getresponse()
+    jsonData = response.read()
 
-    js = json.load(urllib2.urlopen(request))
-    # jd = json.dumps(js)
+    data = json.loads(jsonData)
+    pd  = data["value"][0]["OnPeakDownload"]
+    pu  = data["value"][0]["OnPeakUpload"]
+    opd = data["value"][0]["OffPeakDownload"]
+    opu = data["value"][0]["OffPeakUpload"]
+    sd  = data["value"][0]["StartDate"]
+    ed  = data["value"][0]["EndDate"]
 
-    results = js["value"][0]
+    if args.verbose:
+        print "OnPeakDownload: %s" % (pd)
+        print "OnPeakUpload: %s" % (pu)
+        print "OffPeakDownload: %s" % (opd)
+        print "OffPeakUpload: %s" % (opu)
 
-    for k in results:
-        if "Download" in k:
-            v = str(results[k])
-            line = k + ' is ' + v.rjust(6, '0') + 'GB'
-            newline = "{0} is {1}".format(k, v)
-            print line.rjust(28)
+        print "Start Date: %s" % (sd)
+        print "End Date: %s" % (ed)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Check TekSavvy Cap')
-    parser.add_argument("--cap", help="Cap in GB")
+    parser.add_argument("-c", "--config", help="Alternative config file")
+    parser.add_argument("--cap", help="Your cap in GB")
     parser.add_argument("--API", help="API Key")
     parser.add_argument("--warn", help="Warn ratio, causes nonzero"
-                        " return code")
-    parser.add_argument("-v", "--verbose", help="Show output, don't just use"
-                        " return code")
+                                       " return code")
+    parser.add_argument("-v", "--verbose", action="store_true",
+                        help="Show output, don't just use return code")
 
     args = parser.parse_args()
 
-    checkCap()
+    checkCap(args)
